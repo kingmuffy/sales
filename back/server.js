b673fe78013d5e24adb8c84a5212557742cd3ace
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2023-03-03 23:43:26 +0000

    models

--- back/server.js
+++ back/server.js
@@ -1,7 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
+mongoose.set('strictQuery')
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO)
+    console.log('good!')
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 app.listen(8800, () => {
+  connect()
   console.log('backend server is running')
 })
