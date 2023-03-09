33304795dcbd203c345692d006c54bf3a524bfbb
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2023-03-09 19:30:30 +0000

    ok

--- back/controller/auth.controller.js
+++ back/controller/auth.controller.js
@@ -2,12 +2,15 @@ import User from '../models/user.models.js'
 import bcrypt from 
 export const register = async (req, res) => {
   try {
-    const newUser = new User(req.body)
+    const newUser = new User({
+        username: ""
+    })
+
 
     await newUser.save()
     res.status(201).send('okay created')
   } catch (err) {
-    res.status(500).send('something wrong')
+    res.status(500).send('something went wrong')
   }
 }
 
