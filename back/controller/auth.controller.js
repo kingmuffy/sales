f58cfdb03b7731f61d292256ea73b9f5377a8db0
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2023-03-13 19:51:08 +0000

    jwt

--- back/controller/auth.controller.js
+++ back/controller/auth.controller.js
@@ -15,6 +15,21 @@ export const register = async (req, res) => {
   }
 }
 
-export const login = async (req, res) => {}
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+    })
+    if (!user) return res.status(404).send('user not found')
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password)
+    if (!isCorrect) return res.status(400).send('wrong password')
+
+    const { password, ...info } = user._doc
+    res.status(200).send(info)
+  } catch (err) {
+    res.status(500).send('something wrong')
+  }
+}
 
 export const logout = async (req, res) => {}
